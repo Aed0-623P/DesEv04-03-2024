b0f16f16087e3e4332e658ebdc479d00786be9d3
Aed0-623P <aedo.felipe95@gmail.com>
2024-03-04 15:22:02 -0300

    Funciona con input, como hacerlo con calcular?

--- ong.py
+++ ong.py
@@ -1,6 +1,6 @@
-fact_1 = input("Ingrese el valor para la primer factorial: ")
+fact_1 = float(input("Ingrese el valor para la primer factorial: "))
 prod_1 = input("Ingrese el valor para la primera productoria ")
-fact_2 = input("Ingrese el valor para la segunda factorial: ")
+fact_2 = float(input("Ingrese el valor para la segunda factorial: "))
 
 #calcular factorial
 def fact_i(x):
@@ -10,32 +10,31 @@ def fact_i(x):
         y *= x
         x -= 1
     return y
-            
 
       #calcular productoria
 def prod_i(x):
     y = 1
-    x * y
-    y += 1
-    return
-
-
-#def calcular(x, y ,z ):  #cual es el punto de esto? es contraproducente
-#    fact_i(x)
-#    prod_i(y)
-#    fact_i(z)
-#    return
-
-
+    for num in x:
+        num = int(num)
+        y *=(num)
+    return y
 
 def main():
-    #calcular (fact_1 = 5 ,prod_i = 5 ,fact_2 = 6)  What?  esto puede reemplazar el input al inicio, pero por qué? si pide input y forzarlo,
-        #bueno, filo
     print(f"El factorial de {fact_1} es {fact_i(fact_1)}")
     print(f"La productoria de {prod_1} es {prod_i(prod_1)}")
     print(f"El factorial de {fact_2} es {fact_i(fact_2)}")
 
-main 
 
 if __name__ == "__main__":
     main()
+
+#def calcular(x, y ,z ):  #cual es el punto de esto? es contraproducente
+ #   fact_i(x)
+ #   prod_i(y)
+ #   fact_i(z)
+ #   return
+
+
+#    print(calcular(fact_1 = 5 ,prod_i = 5 ,fact_2 = 6))  #What?  esto puede reemplazar el input al inicio, pero por qué? si pide input y forzarlo,
+        #bueno, filo
+#    calcular(fact_1,prod_1,fact_2)
