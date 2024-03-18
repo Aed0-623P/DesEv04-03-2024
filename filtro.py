d2fc9eb1ab87c8d57488eb53ca850d553155a9dd
Aed0-623P <aedo.felipe95@gmail.com>
2024-03-18 16:48:12 -0300

    Ejercicio actualizado

--- filtro.py
+++ filtro.py
@@ -9,17 +9,15 @@ precios = {
     "Tarjeta de Video": 1500000,
 }
 
-
 def filtrado(diccionario, umbral, v):
-    if v == "menor":
-        menores = ", ".join([k for k, v in diccionario.items() if v < umbral])
-        return f"los productos menores al umbral son: {menores}"
-    elif v == "mayor":
+    if "mayor" in v:
         mayores = ", ".join([k for k, v in diccionario.items() if v > umbral])
         return f"los productos mayores al umbral son: {mayores}"
     else:
-        return "Lo sentimos, la operación no es válida"
+        menores = ", ".join([k for k, v in diccionario.items() if v < umbral])
+        return f"los productos menores al umbral son: {menores}"
 
-
-resultado = filtrado(precios, int(sys.argv[1]), sys.argv[2])
+umbral = int(sys.argv[1])
+v = sys.argv[2] if len(sys.argv) > 2 else "mayor"
+resultado = filtrado(precios, umbral, v)
 print(resultado)
--- ong2.py
+++ ong2.py
@@ -1,10 +1,19 @@
-def calcular(x, y ,z):
-    x2 = fact_i(int(x))
-    y2 = prod_i(y)
-    z2 = fact_i(int(z))
-    print(f"El factorial de {x} es {x2}")
-    print(f"La productoria de {y} es {y2}")
-    print(f"El factorial de {z} es {z2}")
+def calcular(**kwargs):
+    for key, value in kwargs.items():
+        if key == 'x':
+            x = value
+            x2 = fact_i(int(x))
+            print(f"El factorial de {x} es {x2}")
+        elif key == 'y':
+            y = value
+            y2 = prod_i(y)
+            print(f"La productoria de {y} es {y2}")
+        elif key == 'z':
+            z = value
+            z2 = fact_i(int(z))
+            print(f"El factorial de {z} es {z2}")
+        else:
+            break
     return
 
 def fact_i(x):
@@ -16,15 +25,13 @@ def fact_i(x):
     return y2
 
 def prod_i(x):
-        y = 1
-        for num in x:
-            num = int(num)
-            y *=(num)
-        return y
+    y = 1
+    for num in x:
+        num = int(num)
+        y *= num
+    return y
 
 
-#ingresar datos sin el "fact_1 = 5" solo dato
-
-calcular(5,[3,6,4,2,8],6)
+calcular(x=5, y=[3, 6, 4, 2, 8], z=6)
 
 
